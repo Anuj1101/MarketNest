a3c7668e7756ca24fb516e60bfd9fb6e5e158eb8
anuj1101 <kranuj952@gmail.com>
2026-03-08 06:53:11 +0530

    add cors address

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,10 +10,7 @@ const productRoutes = require("./routes/productRoutes")
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(cors())
 app.use(cookieParser())
 
 // Routes
